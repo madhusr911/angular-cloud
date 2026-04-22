96cb124dc43d515edf1796942af2b4cbb0069ab2
MadSup <madsup@MadSups-MacBook-Air.local>
2026-04-22 23:31:14 +0530

    Added a header for Home and Contact

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Contact } from './contact/contact';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'contact', component: Contact },
+  { path: '**', redirectTo: '' },
+];
